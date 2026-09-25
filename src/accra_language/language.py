904d94d7a35f42b1ab7acedfcddcbac4442e51a7
Rumen <rumenmitov@protonmail.com>
2026-09-25 09:42:55 +0200

    language.py: allow for a select environment manager

--- src/accra_language/language.py
+++ src/accra_language/language.py
@@ -11,13 +11,16 @@ from .error import AccraBuildError, AccraInstallError, AccraRunError
 class LanguageSpec(BaseModel):
     name: str
     version: str
-    env_managers: set[EnvironmentManager] = Field(default_factory=set)
+    supported_env_managers: set[EnvironmentManager] = Field(default_factory=set)
     analyzers: set[Analyzer] = Field(default_factory=set)
 
 
 class Language(ABC):
-    def __init__(self, spec: LanguageSpec):
+    def __init__(
+        self, spec: LanguageSpec, env_manager: EnvironmentManager | None = None
+    ):
         self.spec = spec
+        self.env_manager = env_manager or next(iter(spec.supported_env_managers))
 
     @abstractmethod
     def detect_language(self, root: Path) -> bool: ...
